8576652a2b4b6f05b839d61263c4f9a3c53aa529
Glitch (howsmydriving-seattle) <none>
2019-12-30 04:49:09 +0000

    Apparently app-root via babel was causing intermittency. Fixes GlenBikes/howsmydrivingwa#114.

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 const packpath = require('packpath');
 const appRootDir = require('app-root-dir').get();
 const appRootPath = require('app-root-path');
-const appRoot = require('app-root');
 
 import * as path from 'path';
 
@@ -9,31 +8,17 @@ let packpath_parent = packpath.parent() ? packpath.parent() : packpath.self();
 let packpath_self = packpath.self();
 
 console.log(
-  `howsmydriving-twitter:\n - packpath_self: ${packpath_self}\n - packpath_parent: ${packpath.parent()}\n - app-root-dir: ${appRootDir}\n - app-root-path: ${appRootPath}\n - app-root (current): see below\n - app-root (root): see below\n - __dirname: ${__dirname}\n - .: ${path.resolve(
+  `howsmydriving-seattle:\n - packpath_self: ${packpath_self}\n - packpath_parent: ${packpath.parent()}\n - app-root-dir: ${appRootDir}\n - app-root-path: ${appRootPath}\n - app-root (current): see below\n - app-root (root): see below\n - __dirname: ${__dirname}\n - .: ${path.resolve(
     '.'
   )}`
 );
-
-appRoot({
-  directory: '.',
-  success: function(roots) {
-    console.log(`howsmydriving-twitter:\n - app-root (current): ${roots}`);
-  }
-});
-
-appRoot({
-  directory: '/',
-  success: function(roots) {
-    console.log(`howsmydriving-twitter:\n - app-root (root): ${roots}`);
-  }
-});
 
 export const log4js_config_path = path.resolve(
   appRootDir + '/dist/config/log4js.json'
 );
 
 console.log(
-  `howsmydriving-twitter: log4js_config_path:\n - ${log4js_config_path}`
+  `howsmydriving-seattle: log4js_config_path:\n - ${log4js_config_path}`
 );
 
 import { SeattleRegion } from './src/seattle';
